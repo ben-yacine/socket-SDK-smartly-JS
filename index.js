a6c2733481b42ac92124bccf8ceec252daa54976
ben-yacine <hoelath@gmail.com>
2018-10-25 13:18:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,8 @@
  * @date 25/10/2018
  */
 
+var remote_socket_server = 'https://webchat.smartly.ai';
 var Smartly;
-
-var headHTML = document.getElementsByTagName('head')[0];
-var scoketJsLib = document.createElement('script');
-scoketJsLib.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js";
-scoketJsLib.onreadystatechange = smartlyClass;
-scoketJsLib.onload = function () {
-    smartlyClass();
-};
-
-headHTML.appendChild(scoketJsLib);
-
 
 function smartlyClass() {
 
@@ -28,8 +18,12 @@ function smartlyClass() {
 
     Smartly = function (attributs) {
 
+        // Static var
+        Smartly.NEW_DIALOG_SESSION = 'NEW_DIALOG_SESSION';
+        Smartly.NEW_INPUT = 'NEW_INPUT';
+            
         // --- class Attribut's
-        this.socket = io.connect('https://webchat.smartly.ai', {
+        this.socket = io.connect(remote_socket_server, {
             transports: ['websocket']
         });
         this.user_id = typeof attributs.user_id !== 'undefined' ? attributs.user_id : Date.now();
@@ -65,12 +59,11 @@ function smartlyClass() {
 
         /**
          * Get output from API REST.
-         * @method getAnswer
+         * @method sendRequest
          * @param   JSON  param
-         * @param   Function  callback
          * @return json
          */
-        getAnswer: function (param, callback) {
+        sendRequest: function (param) {
             param.user_data.sender = (this.user_id).toString();
             this.socket.emit('new_log', {
                 platform: "webchat",
@@ -79,39 +72,51 @@ function smartlyClass() {
                 lang: this.lang,
                 event_name: param.event_name,
                 user_data: param.user_data,
-                input: param.input
+                input: typeof param.input !== 'undefined' ? param.input : ''
             });
         }
 
     };
 }
 
-// Instantiate object from SmartlyClass
-var objSmartly = new Smartly({
-    skill_id: "5b7a9c169a99d7260bc2c18a",
-    user_id: "userIdcomposed",
-    lang: "fr-fr"
-});
+var headHTML = document.getElementsByTagName('head')[0];
+var scoketJsLib = document.createElement('script');
+scoketJsLib.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js";
+scoketJsLib.onreadystatechange = smartlyClass;
 
-// Event for conversation module messages
-objSmartly.on('onMessage', function (message) {
-    console.log(message)
-});
+scoketJsLib.onload = function () {
+    smartlyClass();
+    
+    // Instantiate object from SmartlyClass
+    var dialog = new Smartly({
+        skill_id: "5b7a9c169a99d7260bc2c18a",
+        user_id: "userIdcomposedOfLettersAndNumbers123456789",
+        lang: "fr-fr"
+    });
+    
+    // Event for conversation module messages
+    dialog.on('onMessage', function (message) {
+        console.log(message)
+    });
 
-// Event for Bot answers
-objSmartly.on('new_log_' + SMARTLY_APP_ID, function (message) {
-    console.log(message);
-});
+    // Event for Bot answers
+    dialog.on('new_log_' + dialog.skill_id, function (message) {
+        console.log(message);
+    });
 
-// Send a NEW_DIALOG_SESSION. Get the message of the welcome state
-objSmartly.getAnswer({
-    event_name: NEW_DIALOG_SESSION,
-    user_data: {},
-});
+    // Example of NEW_DIALOG_SESSION request. Get the message of the welcome state
+    dialog.sendRequest({
+        event_name: Smartly.NEW_DIALOG_SESSION,
+        user_data: {},
+    });
 
-// Send a NEW_INPUT
-objSmartly.getAnswer({
-    event_name: NEW_INPUT,
-    input: "",
-    user_data: {}
-});
+    // Example a NEW_INPUT request
+    dialog.sendRequest({
+        event_name: Smartly.NEW_INPUT,
+        input: "suivant",
+        user_data: {}
+    });
+};
+
+headHTML.appendChild(scoketJsLib);
+
